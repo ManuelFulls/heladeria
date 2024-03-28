8e5116bbe6c3861c3a3f81312f7156d0ab333d05
Betsa-lopez <lopezbetsabe306@gmail.com>
2024-03-27 20:59:11 -0600

    Se especifico que las promociones seran vigentes en semanas

--- src/app/Promocion/page.jsx
+++ src/app/Promocion/page.jsx
@@ -1,3 +1,7 @@
 export default function Promociones() {
-  return <h1>Promociones</h1>;
-}
+  return (<section>
+    <h1>PROMOCIONES</h1>
+    <span>Aqui iran las promociones, no pondremos muchas promociones solo 3 que 
+      sean vigentes a la semana
+    </span>
+    </section>)}
